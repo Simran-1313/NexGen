26ee8bb2d83c2d711c63913acd856b4e6e6307ff
Simran-1313 <singhsimranpreet916@gmail.com>
2024-02-28 01:31:37 +0530

    first commit

---
+++ components/Navbar.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import Image from 'next/image'
+import image from '@/public/IMAGE.png'
+import Link from 'next/link'
+
+const Navbar = () => {
+  return (
+   <div className=' w-full h-[4rem] bg-[#212731] flex justify-center items-center   ;
+   '>
+     <div className="relative w-[258px] ">
+      <input
+        type="text"
+        placeholder=""
+        className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white shadow-sm focus:outline-none focus:bg-white focus:border-blue-500 sm:text-sm"
+      />
+      <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+        <Image src={image} width={24} height={24} alt="search" className="h-5 w-5 text-gray-400" />
+      </div>
+    </div>
+    <div>
+        <Link href='/'  >Categories</Link>
+    </div>
+    <div>
+        <Link href='/' >Website BUilders</Link>
+    </div>
+    <div>
+        <Link href='/' >Today'sdeals</Link>
+    </div>
+   </div>
+  )
+}
+
+export default Navbar
