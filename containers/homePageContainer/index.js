411036a3f9162fbc81edac7b4ac4b5d2595fc9fb
Simran-1313 <singhsimranpreet916@gmail.com>
2024-02-28 20:47:20 +0530

    punneet

--- containers/homePageContainer/index.js
+++ containers/homePageContainer/index.js
@@ -4,6 +4,9 @@ import SoloBoxData from './sections/SoloBoxData';
 import 'tailwindcss/tailwind.css';
 import Section from '@/components/Section';
 import Relateddeals from '@/components/Relateddeals';
+import tick from '@/public/data-img/tick.png'
+import info from '@/public/data-img/info.png'
+import Image from 'next/image';
 function HomePageContainer() {
 
     const { PageData, data } = useContext(DataContext);
@@ -16,7 +19,19 @@ function HomePageContainer() {
     return (
         <>
         <div>
-
+        <div>
+        <h2 className='text-5xl ' >
+        Best Website builders in the US
+        </h2>
+        <hr/>
+        </div>
+        <div>
+           <div className='flex' >
+           <div className='flex'><Image className='h-5 w-5 my-1 mx-2' src={tick}/><p>Last Updated - February 22,2020</p></div>
+           <div className='flex'><Image className='h-5 w-5 my-1 mx-2' src={info}/><p>Advertising Disclosure</p></div>
+           </div>
+        </div>
+        
         </div>
         <div>
 
--- containers/homePageContainer/sections/SoloBoxData.js
+++ containers/homePageContainer/sections/SoloBoxData.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import img from '@/public/data-img/pc.png'
-import img2 from `@{item.rating.rating_image}` 
+
 function SoloBoxData({ item }) {
 
 
@@ -15,7 +15,7 @@ function SoloBoxData({ item }) {
                 {item.main_highlight}
                 {item.rating.number}
                 {item.rating.title}
-                <Image src ={img2} width={100} height={100}/>
+
                
                 
             </div>
